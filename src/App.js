cd7da376e5c32b0f6bb098f85acf01176fc29370
Shaz <shrah.eg.dk>
2023-11-30 07:26:56 +0530

    JSX learnings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './Appa.css';
 
+let name ="shaz";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Shaz
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati illo autem maxime vel reiciendis at numquam, ex amet excepturi dolorem, sunt provident consequatur dolor et est nobis itaque! Quasi, aliquam dolores! Suscipit, velit.</p>
     </div>
+    <div className="blank">Set bro</div>
+
+    </>
   );
 }
-
 export default App;
